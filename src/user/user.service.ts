750902e91c8a43d240fb177559a73f0b902183b8
OrzeFox <dacm1997@yahoo.com>
2023-06-17 00:44:32 +0200

    services

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiTags } from '@nestjs/swagger';
@@ -15,20 +15,53 @@ export class UserService {
   ) {}
   
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const { nombre, apellidos, saldo, password, email } = createUserDto;
+
+    const newUser = this.userRepository.create({
+      nombre,
+      apellidos,
+      saldo,
+      password,
+      email,
+      fecha_creacion: new Date(),
+      fecha_actualizacion: new Date(),
+    });
+
+    return this.userRepository.save(newUser);
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
+    
+    user.nombre = updateUserDto.nombre;
+    user.apellidos = updateUserDto.apellidos;
+    user.saldo = updateUserDto.saldo;
+    user.password = updateUserDto.password;
+    user.email = updateUserDto.email;
+    user.fecha_actualizacion = new Date();
+
+
+    const updatedUser = await this.userRepository.save(user);
+
+    return updatedUser;
   }
+
 
   remove(id: number) {
     return `This action removes a #${id} user`;
