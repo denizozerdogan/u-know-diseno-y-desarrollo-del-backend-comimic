0531342ae18335b1eec2ca4ab27eae754d232e50
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-06-16 14:57:38 +0200

    user and content folders created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ContentModule } from './content/content.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ContentModule],
   controllers: [AppController],
   providers: [AppService],
 })
