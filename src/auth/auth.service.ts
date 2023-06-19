562df5ec10683d6511e2846aee77414d5c3d398f
OrzeFox <dacm1997@yahoo.com>
2023-06-19 17:16:22 +0200

    fix typo

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   ConflictException,
   Injectable,
   InternalServerErrorException,
@@ -10,31 +11,37 @@ import { UserService } from 'src/user/user.service';
 @Injectable()
 export class AuthService {
   constructor(private userService: UserService) {}
+
   async encrypt(password: string): Promise<string> {
     try {
       const salt = await genSalt(10);
       return hash(password, salt);
     } catch (error) {
-      throw new InternalServerErrorException();
+      throw new InternalServerErrorException('Error al encriptar la contraseña');
     }
   }
+
   async register(userObject: RegisterAuthDto) {
     try {
-      const hashedPassword = await this.encrypt(userObject.password);
-    //   console.log(user, hashedPassword);
+      if (!userObject.email || !userObject.password) {
+        throw new BadRequestException('El correo electrónico y la contraseña son requeridos');
+      }
 
+      const hashedPassword = await this.encrypt(userObject.password);
       const createUser = await this.userService.createUser({
         password: hashedPassword,
         ...userObject
       });
-
+      
+      console.log (userObject,hashedPassword)
       const { password, ...rest } = createUser;
+
+      
+
       return rest;
     } catch (error) {
-      if (error?.erno == 1062) {
-        throw ConflictException;
-      }
+      
+      throw new InternalServerErrorException('Error al registrar el usuario');
     }
-
   }
 }
